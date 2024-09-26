57be06c801bcebe48be5f7a3ba64c1e410691286
intmain84 <aheleha@gmail.com>
2024-09-26 10:44:38 +0500

    mapEstablishing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvents,
+} from "react-leaflet";
+import "./index.css";
 
 function App() {
   const position = [51.505, -0.09];
 
   return (
     <div className="map-container">
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={position} zoom={13}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={position}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
+        <ClickPosition />
       </MapContainer>
     </div>
   );
+}
+
+function ClickPosition() {
+  useMapEvents({
+    click: (e) => {
+      console.log(e);
+    },
+  });
+
+  return null;
 }
 
 export default App;
